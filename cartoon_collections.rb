c6d2760a858ad292673403863aa1dcc773fa9165
Brian Kleiner <brian.j.kleiner@gmail.com>
2020-01-23 18:34:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names) 
+  names.each_with_index { |name, idx| puts "#{idx + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |ele| ele.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  snacks.each_with_index do |snack, idx|
+    if cheese_types.include?(snack)
+      return snack[idx]
+    else
+      nil
+    end
+  end
+  
 end
